97fd72cde49f031682c93da5b4847502edd16c04
laine <lainedora9@gmail.com>
2022-06-27 11:12:03 +0200

    update

--- inc/init.php
+++ inc/init.php
@@ -1,7 +1,7 @@
 
 <?php
 // Connexion à la BDD
-$pdo = new PDO('mysql:host=localhost;dbname=boutique','root','', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+$pdo = new PDO('mysql:host=localhost;dbname=boutique','root','root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
 //var_dump($pdo);
 
